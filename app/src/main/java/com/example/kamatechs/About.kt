09d9953a5225e8a36f646a2d41eaf0864dcacc8d
chewipie01 <qcamdelacruz@tip.edu.ph>
2022-12-02 19:35:14 +0800

    FAQ, ABOUT, NAV

--- app/src/main/java/com/example/kamatechs/About.kt
+++ app/src/main/java/com/example/kamatechs/About.kt
@@ -1,4 +1,24 @@
 package com.example.kamatechs
 
-class About {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+
+class About : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        (activity as AppCompatActivity).supportActionBar?.title = "ABOUT"
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+
 }
