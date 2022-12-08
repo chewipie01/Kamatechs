ee41e58f86c7e34246b42023efbaa219e504eff3
Jeanne Margarette Santos <qjmmsantos@tip.edu.ph>
2022-12-08 16:18:08 +0800

    Modified Read Data and Edited Design

--- app/src/main/java/com/example/kamatechs/Room.kt
+++ app/src/main/java/com/example/kamatechs/Room.kt
@@ -1,6 +1,7 @@
 package com.example.kamatechs
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kamatechs.database.Storage
@@ -25,23 +26,32 @@ class Room : AppCompatActivity() {
 
         binding = RoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         appDb = StorageDatabase.getDatabase(this)
 
         binding.btnWriteData.setOnClickListener {
             writeData()
         }
+
         binding.btnReadData.setOnClickListener {
             readData()
         }
 
+        binding.btnDeleteAll.setOnClickListener {
+
+            GlobalScope.launch {
+
+                appDb.storageDao().deleteAll()
+
+            }
+
+        }
     }
 
     private fun writeData(){
 
+        val rollNo = binding.etRollNo.text.toString()
         val temperature = binding.Temp.text.toString()
         val humidity  = binding.Humid.text.toString()
-        val rollNo = binding.etRollNo.text.toString()
 
         if(temperature.isNotEmpty() && humidity.isNotEmpty() && rollNo.isNotEmpty()     ) {
             val storage = Storage(
@@ -52,18 +62,19 @@ class Room : AppCompatActivity() {
                 appDb.storageDao().insert(storage)
             }
 
-            binding.Temp.text.clear()
-            binding.Humid.text.clear()
-            binding.etRollNo.text.clear()
+            binding.Temp.text?.clear()
+            binding.Humid.text?.clear()
+            binding.etRollNo.text?.clear()
 
             Toast.makeText(this@Room,"Successfully written",Toast.LENGTH_SHORT).show()
-        }
-        else Toast.makeText(this@Room,"Please Enter Data",Toast.LENGTH_SHORT).show()
+        }else Toast.makeText(this@Room,"PLease Enter Data",Toast.LENGTH_SHORT).show()
 
     }
+
     private fun readData(){
 
         val rollNo = binding.etRollNoRead.text.toString()
+
         if (rollNo.isNotEmpty()){
 
             lateinit var storage : Storage
@@ -72,24 +83,31 @@ class Room : AppCompatActivity() {
 
                 storage = appDb.storageDao().findByRoll(rollNo.toInt())
                 displayData(storage)
+
+                binding.Temp.text?.clear()
+                binding.Humid.text?.clear()
+                binding.etRollNo.text?.clear()
+
             }
-        }
-        else Toast.makeText(this@Room,"Please enter the data", Toast.LENGTH_SHORT).show()
+
+        }else Toast.makeText(this@Room,"Please enter the data", Toast.LENGTH_SHORT).show()
+
     }
 
     private suspend fun displayData(storage: Storage){
+
         withContext(Dispatchers.Main){
 
-            binding.tvFirstName.text = storage.Temperature.toString()
-            binding.tvLastName.text = storage.Humidity.toString()
             binding.tvRollNo.text = storage.rollNo.toString()
-        }
-    }
+            binding.tvTemp.text = storage.Temperature.toString()
+            binding.tvHumid.text = storage.Humidity.toString()
 
+        }
+
+    }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
 
 }
-
