ba0a6afc72846b24d5dce2d978386099187d74a5
Jeanne Margarette Santos <qjmmsantos@tip.edu.ph>
2022-12-08 12:24:00 +0800

    Added Read Data and Modified Weather API

--- app/src/main/java/com/example/kamatechs/Room.kt
+++ app/src/main/java/com/example/kamatechs/Room.kt
@@ -1,7 +1,6 @@
 package com.example.kamatechs
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kamatechs.database.Storage
@@ -32,18 +31,21 @@ class Room : AppCompatActivity() {
         binding.btnWriteData.setOnClickListener {
             writeData()
         }
+        binding.btnReadData.setOnClickListener {
+            readData()
+        }
 
     }
 
     private fun writeData(){
 
-        val Temperature = binding.Temp.text.toString()
-        val Humidity  = binding.Humid.text.toString()
+        val temperature = binding.Temp.text.toString()
+        val humidity  = binding.Humid.text.toString()
         val rollNo = binding.etRollNo.text.toString()
 
-        if(Temperature.isNotEmpty() && Humidity.isNotEmpty() && rollNo.isNotEmpty()     ) {
+        if(temperature.isNotEmpty() && humidity.isNotEmpty() && rollNo.isNotEmpty()     ) {
             val storage = Storage(
-                null, Temperature.toInt(), Humidity.toInt(), rollNo.toInt()
+                null, temperature.toInt(), humidity.toInt(), rollNo.toInt()
             )
             GlobalScope.launch(Dispatchers.IO) {
 
@@ -56,13 +58,38 @@ class Room : AppCompatActivity() {
 
             Toast.makeText(this@Room,"Successfully written",Toast.LENGTH_SHORT).show()
         }
-        else Toast.makeText(this@Room,"PLease Enter Data",Toast.LENGTH_SHORT).show()
-
+        else Toast.makeText(this@Room,"Please Enter Data",Toast.LENGTH_SHORT).show()
 
     }
+    private fun readData(){
+
+        val rollNo = binding.etRollNoRead.text.toString()
+        if (rollNo.isNotEmpty()){
+
+            lateinit var storage : Storage
+
+            GlobalScope.launch {
+
+                storage = appDb.storageDao().findByRoll(rollNo.toInt())
+                displayData(storage)
+            }
+        }
+        else Toast.makeText(this@Room,"Please enter the data", Toast.LENGTH_SHORT).show()
+    }
+
+    private suspend fun displayData(storage: Storage){
+        withContext(Dispatchers.Main){
+
+            binding.tvFirstName.text = storage.Temperature.toString()
+            binding.tvLastName.text = storage.Humidity.toString()
+            binding.tvRollNo.text = storage.rollNo.toString()
+        }
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
 
 }
+
---
+++ app/src/main/java/com/example/kamatechs/api/Weather.kt
@@ -0,0 +1,78 @@
+package com.example.kamatechs.api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.kamatechs.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
+class Weather: AppCompatActivity() {
+    private var weatherData: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+
+        weatherData = findViewById(R.id.textView)
+        findViewById<View> (R.id.data_button).setOnClickListener {
+            getCurrentData()
+        }
+    }
+
+    private fun getCurrentData() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BaseUrl)
+            .addConverterFactory (GsonConverterFactory.create())
+            .build()
+        val service = retrofit.create(WeatherService::class.java)
+        val call = service.getCurrentWeatherData(lat, lon, AppId)
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(
+                call: Call<WeatherResponse>,
+                response: Response<WeatherResponse>
+            ) {
+                if (response.code() == 200) {
+                    val weatherResponse = response.body()!!
+
+                    val stringBuilder = "Country: " +
+                            weatherResponse.sys!!.country +
+                            "\n" +
+                            "Name: " +
+                            weatherResponse.name +
+                            "\n" +
+                            "Temperature (°C): " +
+                            weatherResponse.main!!.temp +
+                            "\n" +
+                            "Temperature (Min): " +
+                            weatherResponse.main!!.temp_min +
+                            "\n" +
+                            "Temperature (Max): " +
+                            weatherResponse.main!!.temp_max +
+                            "\n" +
+                            "Humidity (%): " +
+                            weatherResponse.main!!.humidity +
+                            "\n" +
+                            "Pressure (hPa): " +
+                            weatherResponse.main!!.pressure
+
+                    weatherData!!.text = stringBuilder
+                }
+            }
+
+            override fun onFailure (call: Call<WeatherResponse>, t: Throwable){
+                weatherData!!.text = t.message
+            }
+        })
+    }
+    companion object {
+        var BaseUrl = "https://api.openweathermap.org/"
+        var AppId = "f813e644951811d2f6ccee45c8492336"
+        var lat = "15.44125"
+        var lon = "120.72863"
+    }
+}
