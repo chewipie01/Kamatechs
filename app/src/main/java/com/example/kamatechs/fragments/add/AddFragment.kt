39ee7302c508086afc6ee74b9b229c1c8caed96f
jmfegues <jmfegues7@gmail.com>
2022-12-12 21:28:18 +0800

    BottomNav Design

--- app/src/main/java/com/example/kamatechs/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/kamatechs/fragments/add/AddFragment.kt
@@ -6,7 +6,6 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
