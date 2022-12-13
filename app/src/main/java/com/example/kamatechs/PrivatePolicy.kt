670b6806ee2851ec51bd3a81fc5144b0bd63e82a
chewipie01 <qcamdelacruz@tip.edu.ph>
2022-12-14 02:55:57 +0800

    Developers and Privacy Policy

--- app/src/main/java/com/example/kamatechs/PrivatePolicy.kt
+++ app/src/main/java/com/example/kamatechs/PrivatePolicy.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class PrivatePolicy: Fragment() {
@@ -13,6 +14,7 @@ class PrivatePolicy: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        (activity as AppCompatActivity).supportActionBar?.title = "PRIVACY POLICY"
         return inflater.inflate(R.layout.activity_privacy, container, false)
     }
 
